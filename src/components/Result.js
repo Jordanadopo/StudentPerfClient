671476a26c31fc204b3886d1ff990319cf059733
jordan.adopo <jordan.adopo@gmail.com>
2021-10-29 22:25:30 +0000

    handling prediction request loader and error

--- src/components/Result.js
+++ src/components/Result.js
@@ -1,24 +1,35 @@
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 export const Result = ({values, reset})=>{
-
+    const prediction = 'The student will ';
     const [res, setRes] = useState({});
     const resultClassName = `resultbox  ${res['predict']}`; 
     const resultStatusClassName = `text-${res['predict']}`;
+    const [isLoading, setIsLoading] = useState(false);
+    const [hasError, setHasError] = useState(false);
 
  
     useEffect(() => {
-        async function fetchPrediction () {
-            const response = await axios({
-                method: 'post',
-                url: 'https://trickydjangoapp.herokuapp.com/classify/',
-                headers: {}, 
-                data: values
-            });
-        
-            setRes(response.data);
-        }
+        const fetchPrediction = async () => {
+            setIsLoading(true);
+            setHasError(false);
+            try {
+                const response = await axios({
+                    method: 'post',
+                    url: 'https://trickydjangoapp.herokuapp.com/classify/',
+                    headers: {}, 
+                    data: values
+                });
+            
+                setRes(await response.data);
+    
+            } catch (error) {
+                setHasError(true);
+            }
+            
+            setIsLoading(false);
+        };
 
         fetchPrediction()
 
@@ -28,12 +39,24 @@ export const Result = ({values, reset})=>{
      
 
     return (
-    <>
-    <h5 className={resultStatusClassName}>RESULT:</h5>
-    <div className={resultClassName}>
-       <h1 className='res'>{res['predict']}</h1>
-    </div>
-    <button className='submit'  onClick={ reset }>{'<  '}Retry</button>
-    </>
+    <React.Fragment>
+     {hasError && <div className='center'> <p className='text-failed'>Something went wrong</p> <button className='submit'  onClick={ reset }>{'<  '}Try another prediction</button></div>}
+      {isLoading ? (
+        <div className='center'>
+            <h3 className='loading'>&#129488; Please wait until the analysis of the information you provided finished &#129488;<br/>You'll the result of prediction soon...</h3>
+            <div class="lds-ring"><div></div><div></div><div></div><div></div></div>
+        </div>
+      ) : (
+        <>
+            <h5 className={resultStatusClassName}>RESULT:</h5>
+            <div className={resultClassName}>
+                <h1 className='res'>{res['predict']==='passed' ? `${prediction} succeed` : `${prediction} fail`}</h1>
+            </div>
+            <button className='submit'  onClick={ reset }>{'<  '}Try another prediction</button>
+        </>
+        )
+    }
+
+    </React.Fragment>
     )
 }
